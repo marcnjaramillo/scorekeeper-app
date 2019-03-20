0b196d2561cf70ace3b2dba2c6d2a47f6ab47f67
marcnjaramillo <marcn.jaramillo@gmail.com>
2019-03-19 19:17:03 -0700

    First section of course progress saved
    Project that is part of the Meteor with React Udemy course.

---
+++ server/main.js
@@ -0,0 +1,10 @@
+import { Meteor } from 'meteor/meteor';
+import {Players} from './../imports/api/players';
+
+Meteor.startup(() => {
+  // Players.insert({
+  //   name: 'Skye',
+  //   score: 12
+  // })
+  // console.log(Players.find().fetch());
+});
